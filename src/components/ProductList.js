311e885b0576555a3182661bc86896d6ff325f43
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-08-01 13:57:45 +0530

    Update ProductList.js

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,1 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function ProductList({ products }) {
+  return (
+    <div>
+      <h2>Available Mobiles</h2>
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>
+            <Link to={`/products/${product.id}`}>
+              {product.name} - ${product.price}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ProductList;
 
