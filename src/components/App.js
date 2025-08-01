f0f2ccaf7a5f20a583466374eb3a47b875173a06
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-08-01 13:56:46 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,48 @@
+import React, { useState } from "react";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import ProductList from "./components/ProductList";
+import ProductDetails from "./components/ProductDetails";
+import AdminPanel from "./components/AdminPanel";
+import initialProducts from "./data/initialProducts";
 
-import React from "react";
-import './../styles/App.css';
+function App() {
+  const [products, setProducts] = useState(initialProducts);
 
-const App = () => {
+  const addProduct = (product) => {
+    setProducts([...products, { ...product, id: Date.now() }]);
+  };
+
+  const updateProduct = (updatedProduct) => {
+    setProducts(products.map(p => p.id === updatedProduct.id ? updatedProduct : p));
+  };
+
+  const deleteProduct = (id) => {
+    setProducts(products.filter(p => p.id !== id));
+  };
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="container">
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/admin">Admin Panel</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<ProductList products={products} />} />
+        <Route path="/products/:id" element={<ProductDetails products={products} />} />
+        <Route
+          path="/admin"
+          element={
+            <AdminPanel
+              products={products}
+              addProduct={addProduct}
+              updateProduct={updateProduct}
+              deleteProduct={deleteProduct}
+            />
+          }
+        />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
